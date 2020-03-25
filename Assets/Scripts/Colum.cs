3ebdee1afee04317c5f3e505e6574f375a14bb37
lalka3710 <vitalzongarkuzo@mail.ru>
2020-03-25 22:10:03 +0300

    commit 1

---
+++ Assets/Scripts/Colum.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class Colum : MonoBehaviour
+{
+    public float speed;
+    void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+   public Rigidbody2D rigidbody;
+    public float force;
+
+    
+   
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            rigidbody.AddForce(Vector2.up * force, ForceMode2D.Impulse);
+        }
+    }
+
+    
+}
